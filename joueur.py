deb779f90117f6b3cc9f3d474e929681ac8cf938
Shqdo <116167859+Shqdo@users.noreply.github.com>
2022-10-19 15:06:22 +0200

    Update joueur.py

--- joueur.py
+++ joueur.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 
 class Joueur:
 
-    def __init__(self):
+    def __init__(self, ecran):
         self.vaisseau = pygame.image.load('vaisseau.png')
         self.vaisseau = pygame.transform.scale(self.vaisseau, (64,64))
         self.rect = self.vaisseau.get_rect()
@@ -14,9 +14,14 @@ class Joueur:
         self.rect.y = 720 - 100
         self.vie = 3
         self.type_tir = None
+        self.screen = ecran
 
     def move_right(self):
         self.rect.x += 5
 
     def move_left(self):
         self.rect.x -= 5
+        
+     def afficher(self):
+        self.screen.blit(self.vaisseau, (self.rect.x, self.rect.y))
+
