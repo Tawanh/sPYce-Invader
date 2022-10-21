00217ae57cccd39faaa7641b5704c1a655f20217
Tawan <108994176+Tawanh@users.noreply.github.com>
2022-10-21 19:49:06 +0200

    class liste_ennemi
    creation d'une classe liste_ennmi pour organiser le main

--- ennemi.py
+++ ennemi.py
@@ -1,8 +1,10 @@
 # Créé par Antoine.Herout
 
-import pygame, sys
+import pygame
 from pygame.locals import *
-import time
+
+from random import randint
+from projectile import Projectile
 
 
 class Ennemi:
@@ -45,3 +47,28 @@ class Ennemi:
 
     def getCoord(self, add_x = 0, add_y = 0):
         return (self._position[0] + add_x,self._position[1] + add_y)
+
+class Liste_ennemi:
+    def __init__(self, nombre_ennemi_horizontal, nombre_etage_ennemi, espacement_horizontal, espacement_vertical, sprite, hauteur_joueur, largeur_joueur, ecran, longueur_deplacement_horizontal):
+        marge = (ecran[0] - (largeur_joueur * nombre_ennemi_horizontal) - (espacement_horizontal *(nombre_ennemi_horizontal))) / 2
+        self._liste_ennemi = []
+        splitted_image = sprite.split(".")
+        for i1 in range(nombre_etage_ennemi):
+            self._liste_ennemi.append([])
+            image = splitted_image[0] + str(i1) + "." + splitted_image[1]
+            image = pygame.image.load(image).convert_alpha()
+            for i2 in range(nombre_ennemi_horizontal):
+                self._liste_ennemi[-1].append(Ennemi(image, 1, hauteur_joueur, largeur_joueur,[marge + (espacement_horizontal + largeur_joueur) * i2 + (espacement_horizontal / 2) - (longueur_deplacement_horizontal / 2), 25 + espacement_vertical * i1]))
+
+    def movement_all_ennemi(self,vitesse,direction,screen,proba,projectile):
+        for i1,j1 in enumerate(self._liste_ennemi):
+            for i2,j2 in enumerate(j1):
+                if not j2 == None:
+                    j2.movement(vitesse,direction,screen)
+                    nombre_none = 0
+                    for i3 in range(i1 + 1, len(self._liste_ennemi)):
+                        if self._liste_ennemi[i3][i2] == None:
+                            nombre_none += 1
+                    if nombre_none == len(self._liste_ennemi) - 1 - i1:
+                        if randint(0,proba) == 1:
+                            projectile.append(Projectile(2 ,j2.getCoord(16),+1, screen))
--- main.py
+++ main.py
@@ -7,22 +7,8 @@ from joueur import Joueur
 from projectile import Projectile
 from boss import Boss
 import sys
-from ennemi import Ennemi
+from ennemi import Ennemi,Liste_ennemi
 from random import randint
-
-#créer une liste d'ennemi
-def listeEnnemi(nb_ennemi,nb_colonne,espacement1,espacement2,sprite,hauteur,largeur,ecran,longueur_deplacement_horizontal):
-    marge = (ecran[0]-(largeur*nb_ennemi)-(espacement1*(nb_ennemi-1)))/ 2
-    liste_ennemi = []
-    splitted_image = sprite.split(".")
-    for i1 in range(nb_colonne):
-        liste_ennemi.append([])
-        image = splitted_image[0] + str(i1) + "." + splitted_image[1]
-        image = pygame.image.load(image).convert_alpha()
-        for i2 in range(nb_ennemi):
-            liste_ennemi[-1].append(Ennemi(image,1,hauteur, largeur,[marge + (espacement1 + largeur) * i2 + (espacement1/2) - (longueur_deplacement_horizontal/2), 25 + espacement2*i1]))
-    return liste_ennemi
-
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -38,7 +24,7 @@ direction = [1,0]
 vitesse = 0.3
 longueur_deplacement_vertical = 20
 longueur_deplacement_horizontal = 100
-liste_ennemi = listeEnnemi(15,4,20,40,"invader.png",11*4,8*4,(longueur,largeur),longueur_deplacement_horizontal)
+liste_ennemi = Liste_ennemi(15,4,20,40,"invader.png",11*4,8*4,(longueur,largeur),longueur_deplacement_horizontal)
 compteur = 0
 
 
@@ -72,33 +58,12 @@ while running:
     #movement ennemis
     compteur +=1
     if paterne_ennemi_horizontal:
-        for i1,j1 in enumerate(liste_ennemi):
-            for i2,j2 in enumerate(j1):
-                if not j2 == None:
-                    j2.movement(vitesse,direction, screen)
-                    nombre_none = 0
-                    for i3 in range(i1 + 1, len(liste_ennemi)):
-                        if liste_ennemi[i3][i2] == None:
-                            nombre_none +=1
-                    if nombre_none == len(liste_ennemi) - 1 - i1:
-                        if randint(0,500) == 1:
-                            projectiles.append(Projectile(2 ,j2.getCoord(16),+1, screen))
+        liste_ennemi.movement_all_ennemi(vitesse,direction,screen,500,projectiles)
         if compteur == longueur_deplacement_horizontal//vitesse:
             paterne_ennemi_horizontal = False
             compteur = 0
     else:
-        for i1,j1 in enumerate(liste_ennemi):
-            for i2,j2 in enumerate(j1):
-                if not j2 == None:
-                    j2.movement(vitesse,[0,1],screen)
-                    nombre_none = 0
-                    for i3 in range(i1 + 1, len(liste_ennemi)):
-                        if liste_ennemi[i3][i2] == None:
-                            nombre_none +=1
-                    if nombre_none == len(liste_ennemi) - 1 - i1:
-                        if randint(0,500) == 1:
-                            projectiles.append(Projectile(2 ,j2.getCoord(16),+1, screen))
-
+        liste_ennemi.movement_all_ennemi(vitesse,[0,1],screen,500,projectiles)
         if compteur == longueur_deplacement_vertical//vitesse:
             paterne_ennemi_horizontal = True
             direction[0] *= -1
