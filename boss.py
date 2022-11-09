62735753958e24696f0dc6c5c333528163cb92a7
Shqdo <116167859+Shqdo@users.noreply.github.com>
2022-11-09 21:38:06 +0100

    Update boss.py
    boss fini avec projectile

--- boss.py
+++ boss.py
@@ -9,7 +9,7 @@ class Boss:
         self.image_boss = pygame.image.load('images/invader2.png')
         self.image_boss = pygame.transform.scale(self.image_boss, (128,128))
         self.rect = self.image_boss.get_rect()
-        self.rect.x = 1080//2
+        self.rect.x = 500
         self.rect.y = 100
         self.screen = ecran
         self._pv = 20
@@ -27,6 +27,10 @@ class Boss:
     def movement(self, longueur):
         self.rect.x += self.vitesse
         self.descente(longueur)
+        x = randint(0,30)
+        if x == 4:
+            projectile.append(Projectile(2 ,(self.rect.x, self.rect.y ),+1, self.screen))
+            
     def getRect(self):
         return self.rect
         
@@ -37,6 +41,7 @@ class Boss:
         if self.rect.x <= 150:
             self.vitesse *= -1
             self.rect.y += 20
+            
     def kill(self):
         self.image_boss = pygame.image.load('images/death2.png')
         self.image_boss = pygame.transform.scale(self.image_boss, (128,128))
